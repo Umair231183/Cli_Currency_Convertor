344e29736ee62f85e573213b2e4a44e365b0fad0
Hafiz_Muhammad_Umair <163179440+Umair231183@users.noreply.github.com>
2024-04-16 08:03:50 +0500

    This is Cli based Currency_convertor
    Currency_Convertor

---
+++ main.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n\tWelcome to 'CodingWithUmair' - Currency Convertor\n"));
+// Define the list of currencies and their exchange rates
+let exchangeRate = {
+    "USD": 1, // Base Currency
+    "EUR": 0.9, // European Currency (Euro)
+    "JYP": 113, // Japenes Currency (Yen)
+    "CAD": 1.3, // Canadian Dollar 
+    "AUD": 1.65, // Australian Dollar 
+    "PKR": 277.97, // Pakistani Rupees
+};
+// Prompt the users to select currencies convert from and to
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency to convert from:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amout",
+        type: "input",
+        message: "Enter the amount to convert:"
+    }
+]);
+// Consion Formula to convert currency
+let fromAmount = exchangeRate[userAnswer.from_currency];
+let toAmount = exchangeRate[userAnswer.to_currency];
+let amount = userAnswer.amout;
+// Formula of conversion
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+// Display the Converted Amount
+console.log(`Converted Amount = ${convertedAmount}`);
